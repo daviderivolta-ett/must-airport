23fda5dfc4b9f1806444075883f2d75957ee55af
daviderivolta-ett <davide.rivolta@grupposcai.it>
2024-02-13 09:13:37 +0100

    Failure tags in validation form

--- src/app/pages/management/validation-form/validation-form.component.ts
+++ src/app/pages/management/validation-form/validation-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TechElementTag } from '../../../models/tech-element-tag.model';
-import { FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ReportParent } from '../../../models/report-parent.model';
 import { KeyValuePipe, NgFor } from '@angular/common';
 import { FailureTag } from '../../../models/failure-tag.model';
@@ -16,7 +16,7 @@ export class ValidationFormComponent {
   public _report: ReportParent = ReportParent.createEmpty();
   @Input() set report(value: ReportParent) {
     if (value && value.id.length > 0) {
-      this._report = value;     
+      this._report = value;
       this.initializeTagForm();
       this.initializeFailureTagsForm();
     }
@@ -26,7 +26,7 @@ export class ValidationFormComponent {
   @Input() set techElementTags(value: TechElementTag[]) {
     if (value && value.length > 0) {
       // console.log(value);
-      this._techElementTags = value;     
+      this._techElementTags = value;
     }
   };
 
@@ -43,14 +43,15 @@ export class ValidationFormComponent {
   public failureTagsForm!: FormGroup;
 
   constructor(private fb: FormBuilder) {
-    this.validationForm = this.fb.group({ });
+    this.validationForm = this.fb.group({});
+
   }
 
   private initializeTagForm(): void {
-    this.tagForm = this.fb.group({ });
-    for (const tag of this._techElementTags) {    
+    this.tagForm = this.fb.group({});
+    for (const tag of this._techElementTags) {
       this.tagForm.addControl(tag.id, new FormControl(this._report.fields.tagTechElement.some((t: TechElementTag | string) => typeof t === 'string' ? t : t.id === tag.id)))
-    } 
+    }
 
     this.validationForm.addControl('tagForm', this.tagForm);
   }
